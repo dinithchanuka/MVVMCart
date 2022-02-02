9a11e688311667b72e786b4cecffba082b8afa02
Dinith Chanuka <dinith@et.lk>
2022-02-02 10:57:14 +0530

    Shopping cart ico and fragment back button

--- app/src/main/java/tqt/triquetra/mvvmcart/views/MainActivity.java
+++ app/src/main/java/tqt/triquetra/mvvmcart/views/MainActivity.java
@@ -1,11 +1,14 @@
 package tqt.triquetra.mvvmcart.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import tqt.triquetra.mvvmcart.R;
 
@@ -20,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
         navController = Navigation.findNavController(this,R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this,navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item,navController)||super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
     }
 }
